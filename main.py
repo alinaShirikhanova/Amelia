bb9e2b76c62c2683e8019a20bf9e801016faa577
alina <alina.meller.98@mail.ru>
2025-03-29 09:43:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# n = 12345
+# while n > 0:
+#     last = n % 10
+#     print(last) # какая-то обработка
+#     n //= 10
+
+# while True:
+#     n = int(input())
+#     if n == 10:
+#         break
+
+# Пользователь вводит числа, пока не введет 10
+# n = int(input())
+# while n != 10:
+#     n = int(input())
+
+
+# Пользователь вводит числа, пока не введет 0.
+# Каждое из чисел неободимо распечатать
+
+# while True:
+#     a = int(input())
+#     if a == 0:
+#         break
+#     print(a)
+
+
+
+# Списки
+# 4 5 5 5
+# m1 = 4
+# m2 = 5
+# m3 = 5
+# m4 = 5
+# print((m1 + m2 + m3 + m4))
+# Список - упорядоченная мультитипная изменяемая коллекция
+# marks = [4, 5, 5, 5] # Создание списка
+# fruits = ['Малина', 'Арбуз', 'Банан', 'Яблоко'] # Создание списка
+# amelia = ['14.09.2011', 'Азимова Амелия Андреевна', 'amelikws', 'амелькинкс', True, 13]
+# marks2 = []
+# lst = list() # пустой список
+# letters = list('amelia')
+# numbers = list(range(1, 10))
+# print(*numbers)
+# print(*letters)
+#
+# print(lst)
+# print(marks)
+# print(*marks)
+# print(amelia[0])
+# print(amelia[3])
+# print(amelia[-1])
+# print(amelia[3])
+#
+
+
+
+n = [1, 2, 3]
+a = []
+s = list()
+f = list('amelikws')
+print(*f)
+
+
+
