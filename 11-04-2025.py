f4e950042973c2e0b1886b0b2ab61a136a6d7956
alina <alina.meller.98@mail.ru>
2025-04-25 19:48:55 +0300

    25-04-2025

--- 11-04-2025.py
+++ 11-04-2025.py
@@ -1,16 +1,16 @@
-lst = [1, 2, 3, 4, 5]
+# lst = [1, 2, 3, 4, 5]
 # print(lst[0])
 # print(lst[1])
 # print(lst[2])
 # print(lst[3])
 # print(len(lst))
-for i in range(len(lst)):
-    print(lst[i])
+# for i in range(len(lst)):
+#     print(lst[i])
 
 
 # Дан список. Вывести все элементы этого списка,
 # которые стоят на четных позициях
-lst = [11, 24, 32, 41, 114]
-
-for i in range(4, -1, -1):
-    print(lst[i])
+# lst = [11, 24, 32, 41, 114]
+#
+# for i in range(4, -1, -1):
+#     print(lst[i])
---
+++ 25-04-2025.py
@@ -0,0 +1,42 @@
+# 1 способ
+lst = []
+# lst = list()
+size = int(input()) #4
+
+for _ in range(size):
+    lst.append(int(input()))
+# print(lst)
+# 2 способ
+# lst = input().split()
+# print(lst)
+
+# t = []
+# t.append(78)
+# print(t)
+# t.append(45)
+# print(t)
+# t.append(100)
+# print(t)
+# t.append(123)
+# print(t)
+
+# Пользователь вводит число n - кол-во элементов в списке
+# А далее на следующих n строках вводит слова. Необходимо
+# добавить все введенные слова в списк
+# и распечатать его
+# l = []
+# r = int(input()) # 5
+# for _ in range(r):
+#     l.append(input())
+
+# l = [1, 2, 3, 4, 5]
+# for i in range(len(l)):
+#     l.append(l[i])
+# print(l)
+
+# l.append(l[0])
+# l.append(l[1])
+# l.append(l[2])
+
+# 1 2 3 4 5
+# 1 2 3 4 5 1 2 3 4 5
