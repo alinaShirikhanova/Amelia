9552a927d0fc86290d85a3ba2adde68327539aba
alina <alina.meller.98@mail.ru>
2025-07-05 13:08:28 +0300

    04-07-2025.py

---
+++ 04-07-2025.py
@@ -0,0 +1,37 @@
+def sum(a, b):
+    return a + b
+
+# параметр - переменная, в которую присваивается переданное в функцию значение
+# аргумент - передаваемое в функцию значение
+
+# return - верни
+
+f = int(input())
+s = int(input())
+t = int(input())
+sum(10, 7)
+
+# print(sum(f, s))
+# print(sum(s, t))
+
+# r1 = sum(f, s)
+# r2 = sum(s, t)
+#
+# if r1 > r2:
+#     print('Сумма первого со вторым больше')
+# else:
+#     print('Сумма второго с третьим больше')
+
+# def print_list(lst):
+#     for i in range(len(lst)):
+#         print(lst[i])
+#
+#
+# names = ['Amelia', 'Fuggler', 'Labubu', 'Хагги Ваги']
+# print_list(names)
+# names.append('Каролина')
+# print_list(names)
+
+
+# Реализовать фнукцию, которая принимает кол-во километров и возвращает кол-во миль
+# мили = километры * 0.6214
